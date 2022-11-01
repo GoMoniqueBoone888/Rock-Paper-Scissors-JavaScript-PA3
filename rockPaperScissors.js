1322067a6625078fee191730fbf7e7d627812b13
Monique Boone <115049831+GoMoniqueBoone888@users.noreply.github.com>
2022-11-01 17:02:53 -0400

    Add files via upload

---
+++ rockPaperScissors.js
@@ -0,0 +1,56 @@
+function playRound(playerSelection, computerSelection) {
+  // your code here to determine whether playerSelection or computerSelection wins!
+  const getComputerChoice= () => {
+    const randomNumber = (Math.floor(Math.random() * 3))
+    if (randomNumber === 0){
+      return 'Rock';
+    }
+    else if (randomNumber === 1){
+      return 'Paper';
+    }
+    else if (randomNumber === 2){
+    return 'Scissors';
+    }
+      }
+  console.log(getComputerChoice());
+  
+}
+
+
+
+function playRound(playerSelection, computerSelection) 
+  if (computerSelection.toLowerCase() === playerSelection.toLowerCase()) {
+     console.log("It's a draw! You both win");
+     return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+  } else if (computerSelection.toLowerCase() === "rock" && playerSelection.toLowerCase() === "scissors") {
+     console.log("Rock beats scissors! You lose ");
+     return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+  } else if (computerSelection.toLowerCase() === "scissors" && playerSelection.toLowerCase() === "rock") {
+     console.log("Rock beats scissors! You win");
+     return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+  } else if (computerSelection.toLowerCase() === "scissors" && playerSelection.toLowerCase() === "paper") {
+     console.log("Scissors beats paper! You lose ");
+     return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+  } else if (computerSelection.toLowerCase() === "paper" && playerSelection.toLowerCase() === "scissors") {
+     console.log("Scissors beats paper! You win");
+     return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+  } else if (computerSelection.toLowerCase() === "paper" && playerSelection.toLowerCase() === "rock") {
+     console.log("Paper beats rock! You lose");
+     return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+  } else if (computerSelection.toLowerCase() === "rock" && playerSelection.toLowerCase() === "paper") {
+     console.log("Paper beats rock! You win");
+     return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+  } else {
+     console.log("I'm not sure what happened, but something went wrong. Please try again");
+  }
+
+
+
+function game() {
+  //Play game 3 times
+  for (let i = 0; i < 3; i++) {
+    // Call playRound function, passing in newly returned values
+    // from the playerPlay and computerPlay functions and log to console
+    console.log(playRound(playerPlay(), computerPlay()));
+  }
+}
