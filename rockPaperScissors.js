d3f035bfe3a1f7508078c9073591ee07535a114e
Monique Boone <quib2015@gmail.com>
2022-11-11 11:32:49 -0500

    Fixed typos

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,6 +1,6 @@
 var playerSelection;                                                                
 var getComputerSelection;
-var playRound;
+
 
 playerSelection = window.prompt("Welcome to the Game! Please choose Rock, Paper or Scissors");
 console.log(playerSelection);
@@ -30,25 +30,25 @@ function playRound(playerSelection, getComputerSelection) {
 
   if (getComputerSelection.toLowerCase() === playerSelection.toLowerCase()) {
      console.log("It's a draw! You both win"); 
-     return(`Your Choice: ${playerSelection}Computer: ${computerSelection}`);
+     return(`Your Choice: ${playerSelection}Computer: ${getComputerSelection}`);
   } else if (getComputerSelection.toLowerCase() === "rock" && playerSelection.toLowerCase() === "scissors") {
      console.log("Rock beats scissors! You lose ");
-     return(`Your Choice: ${playerSelection}Computer: ${computerSelection}`);
+     return(`Your Choice: ${playerSelection}Computer: ${getComputerSelection}`);
   } else if (getComputerSelection.toLowerCase() === "scissors" && playerSelection.toLowerCase() === "rock") {
      console.log("Rock beats scissors! You win");
-     return(`Your Choice: ${playerSelection}Computer: ${computerSelection}`);
+     return(`Your Choice: ${playerSelection}Computer: ${getComputerSelection}`);
   } else if (getComputerSelection.toLowerCase() === "scissors" && playerSelection.toLowerCase() === "paper") {
      console.log("Scissors beats paper! You lose ");
-     return(`Your Choice: ${playerSelection}Computer: ${computerSelection}`);
+     return(`Your Choice: ${playerSelection}Computer: ${getComputerSelection}`);
   } else if (getComputerSelection.toLowerCase() === "paper" && playerSelection.toLowerCase() === "scissors") {
      console.log("Scissors beats paper! You win");
-     return(`Your Choice: ${playerSelection}Computer: ${computerSelection}`);
+     return(`Your Choice: ${playerSelection}Computer: ${getComputerSelection}`);
   } else if (getComputerSelection.toLowerCase() === "paper" && playerSelection.toLowerCase() === "rock") {
      console.log("Paper beats rock! You lose");
-     return(`Your Choice: ${playerSelection}Computer: ${computerSelection}`);
+     return(`Your Choice: ${playerSelection}Computer: ${getComputerSelection}`);
   } else if (getComputerSelection.toLowerCase() === "rock" && playerSelection.toLowerCase() === "paper") {
      console.log("Paper beats rock! You win");
-     return(`Your Choice: ${playerSelection}Computer: ${computerSelection}`);
+     return(`Your Choice: ${playerSelection}Computer: ${getComputerSelection}`);
   } else {
      console.log("I'm not sure what happened, but something went wrong. Please try again");
   }
@@ -60,6 +60,6 @@ function game() {
   //Play game 3 times
   for (let i = 0; i < 3; i++) {
     // from the playerSelection and getComputerSelection functions and log to console
-    console.log(playRound(playerSelectiion(), getComputerSelection()));
+    console.log(playRound(playerSelection(), getComputerSelection()));
   }
 }
